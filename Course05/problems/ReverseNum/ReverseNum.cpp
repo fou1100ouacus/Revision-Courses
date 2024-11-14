5d73dedffa8778fdb9695c39121a8be3ce64d327
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2024-11-14 20:50:03 +0200

    Add files via upload

---
+++ Course05/problems/ReverseNum/ReverseNum.cpp
@@ -0,0 +1,32 @@
+// ReverseNum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number; // 10
+    } while (Number <= 0);
+    return Number;
+}
+void PrintDigit(int digits)//1234
+{
+    while (digits>0)
+    {
+       
+       cout << digits % 10 << endl;
+       digits /= 10;
+
+    }
+
+
+}
+int main()
+{
+    PrintDigit(ReadPositiveNumber("enter"));
+}
+
